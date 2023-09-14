e0375ed47f09ccee2aea290e7c2a88be9a4c10f4
Samara Raquel <samara.raquelmoura@gmail.com>
2023-09-13 21:52:58 -0300

    resolucão das questões

--- Ex14_Sa.js
+++ Ex14_Sa.js
@@ -1,11 +1,21 @@
 /*Questão 14: Implemente um algoritmo que possibilita que você adicione os 5 filmes ou seriados que você mais 
 gostou de assistir. Esse código deve impedir que registros sejam informados em duplicidade.*/
 
-const SerieseFilmes = ["TheBigBangsTheory", "GameofThrones", "Suits", "John Wick", "Sherlock Holmes", "GameofThrones" ];
-SerieseFilmes.sort();
-console.log(SerieseFilmes);
+const filmeseSeriesFavoritos = [];
+function adicionarFilmesouSeries(filme) {
+  if (!filmeseSeriesFavoritos.includes(filme)) {
+    filmeseSeriesFavoritos.push(filme);
+    console.log(`"${filme}" foi adicionado à lista de favoritos.`);
+} else {
+    console.log(`"${filme}" já está na lista de favoritos.`);
+}
+}
+adicionarFilmesouSeries("John Wick");
+adicionarFilmesouSeries("Game of Thrones");
+adicionarFilmesouSeries("Game of Thrones"); 
+adicionarFilmesouSeries("Sherlock Holmes");
+adicionarFilmesouSeries("Suits");
+adicionarFilmesouSeries("The Big Bangs Theory");
 
-const SerieseFilmesSemDuplicidade = SerieseFilmes.filter((valor, indice, self) => {
-    return self.indexOf(valor) === indice;
-  });
-  console.log(SerieseFilmesSemDuplicidade);
+resultado = filmeseSeriesFavoritos;
+console.log(resultado);
--- Ex15_Sa.js
+++ Ex15_Sa.js
@@ -3,3 +3,18 @@ mas não tem a senha, porém, você conhece algumas características que obteve 
  que a senha seja uma dessas palavras “chocolateQuente viajar amoviajar 123456 alterarsenha”.
 a) Desenvolva uma função que receba como parâmetro esse texto inteiro e verifique se a senha do usuário que é 
 amoviajar está contida nessas palavras; Caso sim, a mensagem “Você acessou a conta!”;*/
+
+    var possivelSenha = ["chocolateQuente", "viajar", "amoviajar", "123456", "alterarsenha"]
+    const senhaAcesso = "amoviajar"
+    function acessarInsta() {
+        for (let index = 0; index < possivelSenha.length; index++) {
+            if (possivelSenha[index] == senhaAcesso) {
+                return "Você acessou a conta!";
+            } 
+        }
+                return "Acesso Negado"
+    }
+
+
+    resultado = acessarInsta()
+    console.log(resultado);
