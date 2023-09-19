285548346ece0b4e25a6b561af3ab147e4350495
Samara Raquel <samara.raquelmoura@gmail.com>
2023-09-18 21:23:42 -0300

    atualização das questões

--- Ex12_Sa.js
+++ Ex12_Sa.js
@@ -18,8 +18,8 @@ const nomeCliente = "";
 const emailCliente = "samara@email.com";
 const cpfCliente = "123.456.789-00";
 
-const resultado = cadastroClientes(nomeCliente, emailCliente, cpfCliente);
-console.log(resultado);
+//const resultado = cadastroClientes(nomeCliente, emailCliente, cpfCliente);
+//console.log(resultado);
 
 /*b) - Sua função deve utilizar outra função de validação de email (procure na internet função pronta de validação de email e 
 utilize-a) e caso o parâmetro de email não seja válido, a mensagem “E-mail inválido” deve ser exibida;*/
@@ -59,8 +59,67 @@ function TestaCPF(strCPF) {
     if (Resto != parseInt(strCPF.substring(10, 11) ) ) return 'CPF inválido';
     return true;
 }
-//var strCPF = "09012741432";
-//console.log(TestaCPF(strCPF));
+var strCPF = "09012741432";
+console.log(TestaCPF(strCPF));
 
 /*d) - Não deve ser permitido o cadastro em duplicidade de email e cpf, logo, verifique se o email e cpf já não estão cadastrados.*/
+// Simulando uma lista de clientes já cadastrados (você deve usar um banco de dados real em um ambiente de produção)
+const clientesCadastrados = [
+    {
+        Nome: "Fabio",
+        Email: "fabio@email.com",
+        CPF: "123.456.789-00"
+    },
+    {
+        Nome: "Vinicius",
+        Email: "vinicius@email.com",
+        CPF: "987.654.321-00"
+    }
+];
 
+function validarEmail(email, callback) {
+    setTimeout(function () {
+        const emailExistente = clientesCadastrados.some(cliente => cliente.Email === email);
+        callback(emailExistente);
+    }, 1000); // Simula uma operação assíncrona de validação
+}
+
+
+function validarCpf(cpf, callback) {
+    setTimeout(function () {
+        const cpfExistente = clientesCadastrados.some(cliente => cliente.CPF === cpf);
+        callback(cpfExistente);
+    }, 1000);
+}
+
+// Função de cadastro de cliente com callbacks
+function cadastrarCliente(nome, email, cpf, callback) {
+    validarEmail(email, function (emailExistente) {
+        if (emailExistente) {
+            callback("E-mail já cadastrado");
+        } else {
+            validarCpf(cpf, function (cpfExistente) {
+                if (cpfExistente) {
+                    callback("CPF já cadastrado");
+                } else {
+                    // Se não houver duplicidade, cadastrar o cliente
+                    const cliente = {
+                        Nome: nome,
+                        Email: email,
+                        CPF: cpf
+                    };
+                    clientesCadastrados.push(cliente);
+                    callback(`Cliente ${nome} cadastrado com sucesso!`);
+                }
+            });
+        }
+    });
+}
+
+const nomeCliente = "Vinicius";
+const emailCliente = "vinicius@email.com";
+const cpfCliente = "111.222.333-44";
+
+cadastrarCliente(nomeCliente, emailCliente, cpfCliente, function (resultado) {
+    console.log(resultado);
+});
--- Ex7_Sa.js
+++ Ex7_Sa.js
@@ -3,39 +3,52 @@ Desenvolva o código que imprima na tela se eles esqueceram ou não de comprar l
 Obs: Implemente os testes unitário.
 */
 
+function verificarEsquecimentoDeLaranjas(compras) {
+    var esqueceramLaranjas = false;
+
+    compras.forEach(function (pessoa) {
+        if (pessoa.nome) {
+            var temLaranjas = pessoa.sacola.some(function (item) {
+                return item.fruta === "laranja";
+            });
+            if (!temLaranjas) {
+                console.log(pessoa.nome + " esqueceu das laranjas");
+                esqueceramLaranjas = true;
+            } else {
+                console.log(pessoa.nome + " não esqueceu das laranjas");
+            }
+        }
+    });
+
+    if (!esqueceramLaranjas) {
+        return "Ninguém esqueceu das laranjas";
+    } else {
+        return "Pelo menos uma pessoa esqueceu das laranjas";
+    }
+}
+
 var compras = [
     {
         nome: "Dimitri",
         sacola: [
-            {fruta: "banana", preço: 2.50, qtd: 12},
-            {fruta: "abacaxi", preço: 2.78, qtd: 1},
-            {fruta: "maçãs", preço: 6.94, qtd: 5}
-            ]
+            { fruta: "banana", preço: 2.50, qtd: 12 },
+            { fruta: "abacaxi", preço: 2.78, qtd: 1 },
+            { fruta: "maçãs", preço: 6.94, qtd: 5 }
+        ]
     },
-{
-    nome: "Monique",
-    sacola: [
-            {fruta: "banana", preço: 2.00, qtd: 10},
-            {fruta: "tomate", preço: 5.00, qtd: 6},
-            {fruta: "abacaxi", preço: 5.00, qtd: 2},
-            {fruta: "morango", preço: 10, qtd: 10},
-            {fruta: "laranja", preço: 3.5, qtd: 5}
-    ]
-},
-]
+    {
+        nome: "Monique",
+        sacola: [
+            { fruta: "banana", preço: 2.00, qtd: 10 },
+            { fruta: "tomate", preço: 5.00, qtd: 6 },
+            { fruta: "abacaxi", preço: 5.00, qtd: 2 },
+            { fruta: "morango", preço: 10, qtd: 10 },
+            { fruta: "laranja", preço: 3.5, qtd: 5 }
+        ]
+    },
+    // Adicione mais objetos aqui, se necessário
+];
 
-var EsqueceramLaranjas = false;
+var resultado = verificarEsquecimentoDeLaranjas(compras);
+console.log(resultado);
 
-compras.forEach(function (pessoa) {
-    var temLaranjas = pessoa.sacola.some (function (item){
-        return item.fruta === "laranja"
-    })
-    if (!temLaranjas){
-        console.log(pessoa.nome + " Esqueceu das Laranjas");
-        EsqueceramLaranjas = true;
-    };
-});
- 
-    if (!EsqueceramLaranjas) {
-        console.log("Não esqueceram das Laranjas");
-    }
